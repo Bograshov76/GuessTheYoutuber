c247a481ccd2407938d090395131beb1615578ac
Gilad Bison <gilad@appdome.com>
2018-04-05 00:58:04 +0300

    level displays images in flex

--- app/level.js
+++ app/level.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button, Image } from 'react-native';
 import { Container, Content, Card, CardItem, Body, Header } from 'native-base';
+import Guess from './guess';
 
 export default class Level extends React.Component {
   render() {
     return (
-        <View>
+        <View style={styles.view}>
           {this.props.levelObj.contents.map(content => 
-            <Image key={content.id} source={{uri: 'http://ec2-52-59-249-218.eu-central-1.compute.amazonaws.com:3000/' + content.image_path}} style={{width: 40, height: 40}}/>
+            <Image style={styles.img} key={content.id} source={{uri: 'http://ec2-52-59-249-218.eu-central-1.compute.amazonaws.com:3000/' + content.image_path}}/>
           )}
         </View>
     );
@@ -21,5 +22,15 @@ const styles = StyleSheet.create({
   },
   btn: {
     flex: 1
+  },
+  view: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  img: {
+    width: 70,
+    height: 70,
+    margin: 1
   }
 });
